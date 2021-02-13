3076e71e4beafee8ae226d3e9c4e6524483e840a
Jamie Moore <jbgmoore@gmail.com>
2021-02-13 22:28:03 +1100

    Bump version: 0.6.2 → 0.6.3

--- src/pex/config.py
+++ src/pex/config.py
@@ -12,6 +12,6 @@ class Config:  # pylint: disable=too-few-public-methods
 
     DEBUG = False
     TESTING = False
-    VERSION = "0.6.2"
+    VERSION = "0.6.3"
     DESC = "Python EXample Service"
     COMMIT_SHA = os.getenv("COMMIT_SHA", "")
