640dc9a3fb7e89643c68a7677db9a7aeb01dd747
Jamie Moore <jbgmoore@gmail.com>
2021-02-13 15:38:43 +1100

    automate the commit

--- src/pex/config.py
+++ src/pex/config.py
@@ -8,6 +8,6 @@ class Config(object):
 
     DEBUG = False
     TESTING = False
-    VERSION = "0.5.0"
+    VERSION = "0.5.1"
     DESC = "Python EXample Service"
     COMMIT_SHA = os.getenv("COMMIT_SHA", "")
