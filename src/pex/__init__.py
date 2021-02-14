0b4060b399bbd400793eb46d046b0615a9ebbbf7
Jamie Moore <jbgmoore@gmail.com>
2021-02-14 14:17:05 +1100

    Bump version: 0.6.4 → 0.6.5

--- src/pex/__init__.py
+++ src/pex/__init__.py
@@ -1,2 +1,2 @@
 """This is the PEX module"""
-__version__ = "0.6.4"
+__version__ = "0.6.5"
--- src/pex/config.py
+++ src/pex/config.py
@@ -12,6 +12,6 @@ class Config:  # pylint: disable=too-few-public-methods
 
     DEBUG = False
     TESTING = False
-    VERSION = "0.6.4"
+    VERSION = "0.6.5"
     DESC = "Python EXample Service"
     COMMIT_SHA = os.getenv("COMMIT_SHA", "")
